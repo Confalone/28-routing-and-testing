e69632297e5e771abd16025cedfc181fd7b380af
Tyler Confalone <tconfalone@gmail.com>
2018-08-06 11:11:07 -0700

    landing and dashboard app components added

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,9 @@
+import React, {Component, Fragment} from 'react';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <h1>Dashboard</h1>
+    );
+  }
+}
---
+++ src/components/landing/index.js
@@ -0,0 +1,9 @@
+import React, {Component, Fragment} from 'react';
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <h1>Landing</h1>
+    )
+  }
+}
